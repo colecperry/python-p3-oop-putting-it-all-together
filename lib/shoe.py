2d8300a71d94ba25a16f85b4ca8d60cb099face4
Cole Perry <colecperry@gmail.com>
2023-04-05 17:08:23 -0700

    Done

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Shoe:
-    pass
+    def __init__(self, brand="brand", size=0, material="material", condition="condition"):
+        self.brand = brand
+        self.size = size
+        self.material = material
+        self.condition = condition
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        if (type(size) == int):
+            self._size = size
+        else:
+            print("size must be an integer")
+        
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+ipdb.set_trace()
+
